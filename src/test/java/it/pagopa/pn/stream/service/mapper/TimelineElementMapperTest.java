c942b8266938112459fc2e4be516473d7538baa7
Stefano Rughetti <stefano.rughetti@emeal.nttdata.com>
2025-01-24 12:40:44 +0100

    [PN-13516] Feat: Improved coverage for deanonimization tests

---
+++ src/test/java/it/pagopa/pn/stream/service/mapper/TimelineElementMapperTest.java
@@ -0,0 +1,45 @@
+package it.pagopa.pn.stream.service.mapper;
+
+import it.pagopa.pn.stream.dto.timeline.TimelineElementInternal;
+import it.pagopa.pn.stream.exceptions.PnStreamException;
+import it.pagopa.pn.stream.generated.openapi.server.v1.dto.TimelineElementV26;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+class TimelineElementMapperTest {
+
+    @Test
+    void internalToExternal_validInput() {
+        TimelineElementInternal internalDto = buildTimelineElementInternal();
+        TimelineElementV26 result = TimelineElementMapper.internalToExternal(internalDto);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(internalDto.getElementId(), result.getElementId());
+        Assertions.assertEquals(internalDto.getTimestamp(), result.getTimestamp());
+    }
+
+    @Test
+    void internalToExternal_invalidJson() {
+        TimelineElementInternal internalDto = buildInvalidTimelineElementInternal();
+        Assertions.assertThrows(PnStreamException.class, () -> {
+            TimelineElementMapper.internalToExternal(internalDto);
+        });
+    }
+
+    private TimelineElementInternal buildTimelineElementInternal() {
+        return TimelineElementInternal.builder()
+                .elementId("001")
+                .timestamp(Instant.parse("2023-10-10T10:00:00Z"))
+                .details("{\"normalizedAddress\":{\"address\":\"VIA SENZA NOME\",\"addressDetails\":\"SCALA B\",\"at\":\"Presso\",\"foreignState\":\"ITALIA\",\"municipality\":\"COSENZA\",\"municipalityDetails\":\"COSENZA\",\"province\":\"CS\",\"zip\":\"87100\"},\"oldAddress\":{\"address\":\"Via senza nome\",\"addressDetails\":\"scala b\",\"at\":\"Presso\",\"foreignState\":\"ITALIA\",\"municipality\":\"Cosenza\",\"municipalityDetails\":\"Cosenza\",\"province\":\"CS\",\"zip\":\"87100\"},\"recIndex\":0}")
+                .build();
+    }
+
+    private TimelineElementInternal buildInvalidTimelineElementInternal() {
+        return TimelineElementInternal.builder()
+                .elementId("001")
+                .timestamp(Instant.parse("2023-10-10T10:00:00Z"))
+                .details("{invalidJson}")
+                .build();
+    }
+}
